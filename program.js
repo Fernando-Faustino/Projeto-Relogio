4b04053e1cc92c037793642c670018ab3584dff6
Fernando Faustino <Fernandofaustino627@gmail.com>
2023-03-06 23:32:51 -0300

    Criado um Projeto de um Relógio

---
+++ program.js
@@ -0,0 +1,30 @@
+const myFavorite = 8;
+console.log("Meu número favorito é: " + myFavorite);
+if (myFavorite === 8) {
+  let myFavorite = 20;
+  console.log("Meu número favorito é: " + myFavorite);
+}
+
+/*const hoursHand = document.querySelector(".hand.hours");
+const minutesHand = document.querySelector(".hand.minutes");
+const secondsHand = document.querySelector(".hand.seconds");
+
+const setRotation = (element, rotationPercentage) => {
+  element.style.setProperty("--rotation", rotationPercentage * 360);
+};
+
+const setClock = () => {
+  const currentDate = new Date();
+
+  const secondsPercentage = currentDate.getSeconds() / 60;
+  const minutesPercentage = (secondsPercentage + currentDate.getMinutes()) / 60;
+  const hoursPercentage = (minutesPercentage + currentDate.getHours()) / 12;
+
+  setRotation(secondsHand, secondsPercentage);
+  setRotation(minutesHand, minutesPercentage);
+  setRotation(hoursHand, hoursPercentage);
+};
+
+setClock();
+
+setInterval(setClock, 1000);*/
